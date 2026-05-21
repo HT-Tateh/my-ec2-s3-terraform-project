84f727bbe852b2a9d9ee2e323da87d3bc15ac72a
Hilary Tebi Tateh <tebihilary27@gmail.com>
2026-05-21 12:58:17 -0400

    Delete ingress and egress rules from main.tf
    Removed ingress and egress rules from the security group.

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+ 
 
 # S3 Bucket
 resource "aws_s3_bucket" "main" {
